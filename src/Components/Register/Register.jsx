65eb646c7450cf2ac9f776c2ed20f2b1735d16b1
Maryam M El-Kassaby <132655031+Maryam-Elkassaby@users.noreply.github.com>
2024-07-06 16:58:04 +0300

    commit

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import Style from './Register.module.css';
 import { useFormik } from 'formik';
+import * as yup from "yup";
 
 export default function Register() {
 
     function submitForm (values){
-        // console.log('hello in form');
         console.log(values);
     }
-    let Formik = useFormik({
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-])|(\\([0-9]{2,3}\\)[ \\-])|([0-9]{2,4})[ \\-])?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+    let validationSchema = yup.object({
+        name : yup
+                .string()
+                .required('Please Enter a username'),
+        email : yup
+                .string()
+                .required('Please Enter your Email'),
+        phone : yup
+                .string()
+                .required('Please Enter your password')
+                .matches(phoneRegExp , 'Phone number is not valid'),
+        password : yup
+                .string()
+                .required('Please Enter your password')
+                .matches(
+                    "^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$",
+                    "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+                ),
+        rePassword : yup
+                .string()
+                .required('Please Enter your rePassword')
+                .oneOf([yup.ref("password") , ''] , "Passwords must match"),
+    })
 
+    let Formik = useFormik({
         initialValues : {
             name : '', 
             email : '' ,
@@ -17,6 +41,7 @@ export default function Register() {
             password : '' , 
             rePassword : ''
         },
+        validationSchema : validationSchema,
         onSubmit : submitForm
     })
 
@@ -31,8 +56,9 @@ export default function Register() {
                     value={Formik.values.name}
                     onChange={Formik.handleChange}
                     onBlur={Formik.handleBlur}
-                />
-
+                />   
+                {Formik.errors.name && Formik.touched.name ? <div class="alert alert-danger mt-2 py-1"> {Formik.errors.name} </div> : ''}
+                
                 <label className='form-label text-main fw-bold d-block text-start mt-4' htmlFor="email">Email</label>
                 <input 
                     className='form-control'
@@ -42,6 +68,7 @@ export default function Register() {
                     onChange={Formik.handleChange}
                     onBlur={Formik.handleBlur}
                 />
+                {Formik.errors.email && Formik.touched.email ? <div class="alert alert-danger mt-2 py-1"> {Formik.errors.email} </div> : ''}
 
                 <label className='form-label text-main fw-bold d-block text-start mt-4' htmlFor="phone">phone</label>
                 <input 
@@ -52,6 +79,7 @@ export default function Register() {
                     onChange={Formik.handleChange}
                     onBlur={Formik.handleBlur}
                 />
+                {Formik.errors.phone && Formik.touched.phone ? <div class="alert alert-danger mt-2 py-1"> {Formik.errors.phone} </div> : ''}
 
                 <label className='form-label text-main fw-bold d-block text-start mt-4' htmlFor="password">password</label>
                 <input 
@@ -62,6 +90,7 @@ export default function Register() {
                     onChange={Formik.handleChange}
                     onBlur={Formik.handleBlur}
                 />
+                {Formik.errors.password && Formik.touched.password ? <div class="alert alert-danger mt-2 py-1"> {Formik.errors.password} </div> : ''}
 
                 <label className='form-label text-main fw-bold d-block text-start mt-4' htmlFor="rePassword">Re-Password</label>
                 <input 
@@ -72,8 +101,9 @@ export default function Register() {
                     onChange={Formik.handleChange}
                     onBlur={Formik.handleBlur}
                 />
-                
-                <button className='btn bg-main text-white mt-3' type='submit'>Register</button>
+                {Formik.errors.rePassword && Formik.touched.rePassword ? <div class="alert alert-danger mt-2 py-1"> {Formik.errors.rePassword} </div> : ''}
+
+                <button  className='btn bg-main text-white mt-3' type='submit'>Register</button>
             </form>
         </div>
     </>
